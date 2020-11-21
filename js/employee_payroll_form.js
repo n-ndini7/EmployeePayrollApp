9b276191fe016ef24a76ece6ba20f7a84542373e
n-ndini7 <hp@RJs-PC>
2020-11-21 13:00:10 +0530

    [Nandini] Added DAy 40 CURD operation UC2 - update employee payroll details

--- js/employee_payroll_form.js
+++ js/employee_payroll_form.js
@@ -1,3 +1,5 @@
+let isUpdate = false;
+ let employeePayrollObj = {};
 window.addEventListener('DOMContentLoaded',(event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
@@ -24,6 +26,7 @@ window.addEventListener('DOMContentLoaded',(event) => {
         else
        dateError.textContent = "Invalid Start date ";
     }
+    checkForUpdate();
 });
     //added event listener to salary to display appropriate value 
     function salaryInput(){
@@ -70,7 +73,7 @@ function save(){
    alert("Thanks! Your form is submitted successfully!" + "\n "+employeepayrollData.toString());
    console.log(employeepayrollData);
    createAndUpdateStorage(employeepayrollData);
-  }
+  };
   function createAndUpdateStorage(employeepayrollData){
       let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
       if(employeePayrollList != undefined){
@@ -92,16 +95,69 @@ function save(){
     document.querySelector("#year").value = 2020;
     document.querySelector("#notes").value= "";
     document.querySelector(".date-error").textContent = "";
-}
+};
 
 const unsetSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
     allItems.forEach(item=>{
         item.checked = false;
     });
-}
+};
+const setSelectedIndex = (id,index) => {
+const element = document.querySelector(id);
+element.selectedIndex = index;
+};
 function currencyConvertorToINR($number ) {
     return (isNaN(parseInt($number))) ?  0 : '₹ ' +  parseInt($number).toLocaleString('en-IN')
   }
 //console.log(currencyConvertorToINR(1000000));
 //localStorage.clear();
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson? true:false;
+    if(!isUpdate) return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();   
+};
+const setValue = (id,value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+};
+const setTextValue = (id,value) => {
+const element = document.querySelector(id);
+element.textContent = value;
+};
+const setForm = () =>{
+    document.querySelector('#name').value = employeePayrollObj._name;
+    setSelectedValues('[name=profile]',employeePayrollObj._profile);
+    setSelectedValues('[name=gender]',employeePayrollObj._gender);
+    setSelectedValues('[name=department]',employeePayrollObj._department);
+    console.log(employeePayrollObj._department);
+    let sal = employeePayrollObj._salary.slice(1);
+    document.querySelector("#salary").value = sal;
+    document.querySelector(".salary-output").textContent = sal;
+    document.querySelector('#notes').value = employeePayrollObj._notes;
+    let date = stringifyDate(employeePayrollObj._startDate).split("/");
+    document.querySelector('#day').value = date[0];
+    document.querySelector('#month').value = date[1];
+    document.querySelector('#year').value = date[2];
+}
+
+const stringifyDate = (date)=> {
+    const options = {day: 'numeric', month: 'numeric', year:'numeric'};
+    const newDate = !date ? "undefined":new Date(Date.parse(date)).toLocaleDateString('en-IN',options);
+    return newDate;
+}
+
+const setSelectedValues = (propertyValue,value) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        if(Array.isArray(value)){
+            if(value.includes(item.value)){
+                item.checked = true;
+            }
+        }
+        else if(item.value == value)
+            item.checked = true;
+    });
+}
--- js/home.js
+++ js/home.js
@@ -4,11 +4,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // console.log(empPayrollList);
    document.querySelector(".emp-count").textContent = empPayrollList.length;
    createInnerHtml();
-   //localStorage.removeItem('editemp');
+   localStorage.removeItem('editEmp');
  });
+ 
 const getEmployeePayrollDataFromStorage = () => {
     return localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
-}
+};
  const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
     if(empPayrollList.length == 0) return;
@@ -24,13 +25,13 @@ const getEmployeePayrollDataFromStorage = () => {
                     <td>${employeePayrollData._startDate}</td>
                     <td>
                         <img name="${employeePayrollData._name}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                        <img name="${employeePayrollData._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                        <img name="${employeePayrollData._name}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
                     </td>
                 </tr>
     `;
     }
     document.querySelector("#table-display").innerHTML = innerHtml;
- }
+ };
  const remove = (node) => {
      var answer = window.confirm("Are you sure you want to delete the details of "+node.name+" from database ?");
    if(answer){
@@ -46,11 +47,19 @@ const getEmployeePayrollDataFromStorage = () => {
    else {
        location.reload();
    }
-}
+};
+const update = (node) => {
+    console.log(node.name);
+    let employeePayrollData = empPayrollList.find(empData => empData._name == node.name)
+    console.log(employeePayrollData);
+    if(!employeePayrollData) return;
+    localStorage.setItem('editEmp',JSON.stringify(employeePayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+};
  const getDeptHtml = (deptList) => {
      let deptHtml = '';
      for(const dept of deptList){
          deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
      }
      return deptHtml;
- }
+ };
