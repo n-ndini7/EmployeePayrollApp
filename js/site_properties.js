eab87a9b2d4d234f31d628afa9136db7fe75a1a2
n-ndini7 <hp@RJs-PC>
2020-11-23 23:38:17 +0530

    [Nandini] AJAX UC4 - make REST call to json server to get all employee payroll data

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/employee_payroll_home.html",
     add_emp_payroll_page: "../pages/emp_payroll_form.html",
      server_url: "http://localhost:3000/EmployeePayrollDB/"
